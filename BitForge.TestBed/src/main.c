75041bb77cc14fae42a31255479ae3cc3e423b92
John Vasiliev <va3iliev.ip@gmail.com>
2025-04-02 15:23:37 +0300

    Platform specific functions

--- BitForge.TestBed/src/main.c
+++ BitForge.TestBed/src/main.c
@@ -1,5 +1,6 @@
 #include <core/logger.h>
 #include <core/asserts.h>
+#include <platform/platform.h>
 
 int main(void) {
   BFFATAL("Hello, %s", "World!");
@@ -9,8 +10,13 @@ int main(void) {
   BFDEBUG("Hello, %s", "World!");
   BFTRACE("Hello, %s", "World!");
 
-  BFASSERT(false);
-  BFASSERT(1 == 0);
+  platform_state state;
+  if (platform_startup(&state, "BitForge Testbed", 100, 100, 1280, 720)) {
+    while (true) {
+      platform_pump_messages(&state);
+    }
+  }
+  platform_shutdown(&state);
 
   return 0;
 }
