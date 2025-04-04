a057b1bcdaa4d342d7c888749d3d9896621a92cc
John Vasiliev <va3iliev.ip@gmail.com>
2025-04-04 17:31:47 +0300

    Vulkan scaffolding

--- BitForge.Engine/src/core/input.c
+++ BitForge.Engine/src/core/input.c
@@ -75,7 +75,7 @@ void input_process_mouse_move(i16 x, i16 y) {
   // Only process if actually different
   if (state.mouse_current.x != x || state.mouse_current.y != y) {
     // NOTE: Enable this if debugging.
-    BFDEBUG("Mouse pos: %i, %i!", x, y);
+    // BFDEBUG("Mouse pos: %i, %i!", x, y);
 
     // Update internal state.
     state.mouse_current.x = x;
