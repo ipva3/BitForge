29521147965b2c9162c11dff972b9272cc785652
John Vasiliev <va3iliev.ip@gmail.com>
2025-04-07 12:18:30 +0300

    Vulkan logican & physical devices

--- BitForge.Engine/src/core/bstring.h
+++ BitForge.Engine/src/core/bstring.h
@@ -7,3 +7,6 @@
 BFAPI u64 string_length(const char* str);
 
 BFAPI char* string_duplicate(const char* str);
+
+// Case-sensetive string comparison. `true` if the same, otherwise `false`.
+BFAPI b8 strings_equal(const char* str0, const char* str1);
